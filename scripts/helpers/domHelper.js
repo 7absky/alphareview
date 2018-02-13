b291a8753c32ef96f4b7ed5738032d08c45138d3
Patryk Laba <patryk@labski.it>
2018-02-13 21:06:34 +0100

    created helper class for DOM manipulation

---
+++ scripts/helpers/domHelper.js
@@ -0,0 +1,30 @@
+define([],function(){
+    
+    function createElement(elt, attributes){
+        var node = document.createElement(elt);
+        // set attributes for DOM element
+        if(attributes) {
+            for(var attr in attributes) {
+                if(attributes.hasOwnProperty(attr)) {
+                    node.setAttribute(attr, attributes[attr]);
+                }
+            }
+        }
+        // check if another argument is a string
+        // if so, append it to the node as a text
+        for (var i = 2; i < arguments.length; i++) {
+            var child = arguments[i];
+            if (typeof child == 'string') {
+                child = document.createTextNode(child);
+            }
+            node.appendChild(child);
+        }
+
+        return node;
+    }
+
+
+    return {
+        createElement: createElement
+    }
+});
